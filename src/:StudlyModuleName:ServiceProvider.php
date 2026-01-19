5c55367fbad1b28bb11ab7350d19b0da394370b0
kid <kidiatoliny@akira-io.com>
2026-01-19 21:45:07 +0100

    feat: add example NavItem and NavGroup to ServiceProvider

--- src/:StudlyModuleName:ServiceProvider.php
+++ src/:StudlyModuleName:ServiceProvider.php
@@ -6,6 +6,8 @@ namespace :VendorNamespace:\:StudlyModuleName:;
 
 use Hunter\Module\Contracts\ModuleServiceProvider;
 use Hunter\Module\Module\Module;
+use Hunter\Module\Navigation\NavGroup;
+use Hunter\Module\Navigation\NavItem;
 use Override;
 
 final class :StudlyModuleName:ServiceProvider extends ModuleServiceProvider
@@ -18,6 +20,27 @@ final class :StudlyModuleName:ServiceProvider extends ModuleServiceProvider
             ->name(':module_name')
             ->description(':package_description')
             ->hasConfig()
-            ->hasRoutes();
+            ->hasRoutes()
+            ->navigation([
+                NavItem::make(':module_name')
+                    ->route(':module_slug.index')
+                    ->icon('LayoutGrid')
+                    ->order(10),
+
+                NavGroup::make(':module_name Group')
+                    ->icon('FolderOpen')
+                    ->order(20)
+                    ->items([
+                        NavItem::make('Overview')
+                            ->route(':module_slug.index')
+                            ->icon('Home')
+                            ->order(1),
+
+                        NavItem::make('Settings')
+                            ->route(':module_slug.index')
+                            ->icon('Settings')
+                            ->order(2),
+                    ]),
+            ]);
     }
 }
